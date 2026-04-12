9ffc64451848010fa68500a9fa828ffb5a2d6098
TonyStark-max <206331729+TonyStark-max@users.noreply.github.comsomuthupakula2@gmail.com>
2026-04-12 17:36:46 +0530

    Initial monorepo setup

---
+++ alert-service/src/test/java/com/log_system/alerts_system/AlertsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log_system.alerts_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
